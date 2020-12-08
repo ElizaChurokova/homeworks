34076a34e0f47bb3894a32f7da6ee28adb4680c1
ElizaChurokova <elizachurokova@gmail.com>
2020-12-08 16:18:36 +0600

    homework 18 a new

---
+++ homework18a/src/com/company/Course.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Course {
+
+    String name;
+    String date;
+    String id;
+    String nameOfProfessor;
+
+    public Course(){}
+
+    public Course(String name, String date, String id, String nameOfProfessor){
+        this.name = name;
+        this.date = date;
+        this.id = id;
+        this.nameOfProfessor = nameOfProfessor;
+    }
+
+    @Override
+    public String toString(){
+        String message = "This course is " + this.name + ".";
+        return message;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setNameOfProfessor(String nameOfProfessor){
+        this.nameOfProfessor = nameOfProfessor;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getNameOfProfessor(){
+        return nameOfProfessor;
+    }
+
+}
+
---
+++ homework18a/src/com/company/Person.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Person {
+    LocalDate dateBirth;
+    String surname;
+    String name;
+    char gender;
+    int age;
+
+    public Person(){}
+
+    public Person(LocalDate dateBirth, String surname, String name, char gender, int age) {
+        this.dateBirth = dateBirth;
+        this.surname = surname;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public LocalDate getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(LocalDate dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
